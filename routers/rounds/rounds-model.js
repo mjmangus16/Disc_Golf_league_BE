810c59e1b6e4d2a42dc05806d122f9a4410dd766
Michael Mangus <mjmangus16@yahoo.com>
2019-11-14 09:56:52 -0500

    created a route for adding a round to a league

--- routers/rounds/rounds-model.js
+++ routers/rounds/rounds-model.js
@@ -2,13 +2,33 @@ const db = require("../../data/dbConfig");
 
 module.exports = {
   getRounds,
-  getRoundsByLeague
+  getRoundsByLeague,
+  addRound
 };
 
 function getRounds() {
   return db("rounds").select("*");
 }
 
+function getRoundById(round_id) {
+  return db("rounds").where("round_id", round_id);
+}
+
 function getRoundsByLeague(league_id) {
   return db("rounds").where("league_id", league_id);
+}
+
+async function addRound(newRound, league_id) {
+  const rounds = await getRoundsByLeague(league_id);
+  const filtered = await rounds.filter(r => r.type === newRound.type);
+
+  return db("rounds")
+    .insert(
+      { ...newRound, league_id, round_num: filtered.length + 1 },
+      "round_id"
+    )
+    .then(round => {
+      const [id] = round;
+      return getRoundById(id).first();
+    });
 }
--- routers/rounds/rounds-router.js
+++ routers/rounds/rounds-router.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const dbRounds = require("./rounds-model");
+const dbLeagues = require("../leagues/leagues-model");
+const restricted = require("../../middleware/restricted");
+const restrictedAdmin = require("../../middleware/restrictedAdmin");
+const checkLeagueOwner = require("../../middleware/checkLeagueOwner");
 
 router.get("/test", (req, res) => {
   res.send("The rounds router is working!");
@@ -29,30 +33,82 @@ router.get("/", (req, res) => {
 // ROUTE:   /api/rounds/league/:league_id
 // DESCRIPTION: Gets all rounds data for a specific league
 
-router.get("/league/:league_id", (req, res) => {
-  dbRounds
-    .getRoundsByLeague(req.params.league_id)
-    .then(rounds => {
-      if (rounds) {
-        const container = rounds.map(round => {
-          return {
-            round_id: round.round_id,
-            date: round.date,
-            type: round.type,
-            round_num: round.round_num
-          };
-        });
-        res.status(200).json(container);
-      } else {
-        res.status(500).json({
-          error: "There have not been any rounds added to this league yet."
-        });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: "Server error getting rounds" });
+router.get("/league/:league_id", async (req, res) => {
+  const league = await dbLeagues.getLeagueById(req.params.league_id);
+
+  if (league) {
+    dbRounds
+      .getRoundsByLeague(req.params.league_id)
+      .then(rounds => {
+        if (rounds.length > 0) {
+          const container = rounds.map(round => {
+            return {
+              round_id: round.round_id,
+              date: round.date,
+              type: round.type,
+              round_num: round.round_num
+            };
+          });
+          res.status(200).json(container);
+        } else {
+          res.status(500).json({
+            error: "There have not been any rounds added to this league yet."
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error getting rounds" });
+      });
+  } else {
+    res.status(500).json({
+      error:
+        "We can not get rounds for a league that does not exist in our database."
     });
+  }
+});
+
+// TYPE:  POST
+// ROUTE:   /api/rounds/add/league/:league_id
+// DESCRIPTION: Adds a round to the league
+
+router.post("/add/league/:league_id", restrictedAdmin, async (req, res) => {
+  const league = await dbLeagues.getLeagueById(req.params.league_id);
+
+  if (league) {
+    if (checkLeagueOwner(league.owner_id, req.jwt.user_id)) {
+      dbRounds
+        .addRound(req.body, req.params.league_id)
+        .then(success => {
+          if (success) {
+            res.status(200).json({
+              success: `We succussfully added a round to the ${league.name} League.`
+            });
+          } else {
+            res.status(500).json({
+              error:
+                "We were unable to create a round for this league. Please try again later."
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({
+            error:
+              "We were unable to create a round for this league. Please try again later."
+          });
+        });
+    } else {
+      res.status(500).json({
+        error: "Only the manager of this league can add a round."
+      });
+    }
+  } else {
+    res.status(500).json({
+      error:
+        "We can not add a round to a league that does not exist in our database."
+    });
+  }
 });
 
 module.exports = router;
