2a67a74de4c5b0b5868b2a33b6bb4f260b6144e8
Michael Mangus <mjmangus16@yahoo.com>
2019-11-14 08:55:06 -0500

    beginning to work on rounds router and model

--- routers/rounds/rounds-model.js
+++ routers/rounds/rounds-model.js
@@ -1,9 +1,14 @@
 const db = require("../../data/dbConfig");
 
 module.exports = {
-  getRounds
+  getRounds,
+  getRoundsByLeague
 };
 
 function getRounds() {
   return db("rounds").select("*");
+}
+
+function getRoundsByLeague(league_id) {
+  return db("rounds").where("league_id", league_id);
 }
--- routers/rounds/rounds-router.js
+++ routers/rounds/rounds-router.js
@@ -1,17 +1,52 @@
 const router = require("express").Router();
-const db = require("./rounds-model");
+const dbRounds = require("./rounds-model");
 
 router.get("/test", (req, res) => {
   res.send("The rounds router is working!");
 });
 
+// TYPE:  GET
+// ROUTE:   /api/rounds/
+// DESCRIPTION: Gets all rounds data
+
 router.get("/", (req, res) => {
-  db.getRounds()
+  dbRounds
+    .getRounds()
     .then(rounds => {
       if (rounds.length > 0) {
         res.status(200).json(rounds);
       } else {
         res.status(500).json({ error: "There are no rounds available" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Server error getting rounds" });
+    });
+});
+
+// TYPE:  GET
+// ROUTE:   /api/rounds/league/:league_id
+// DESCRIPTION: Gets all rounds data for a specific league
+
+router.get("/league/:league_id", (req, res) => {
+  dbRounds
+    .getRoundsByLeague(req.params.league_id)
+    .then(rounds => {
+      if (rounds) {
+        const container = rounds.map(round => {
+          return {
+            round_id: round.round_id,
+            date: round.date,
+            type: round.type,
+            round_num: round.round_num
+          };
+        });
+        res.status(200).json(container);
+      } else {
+        res.status(500).json({
+          error: "There have not been any rounds added to this league yet."
+        });
       }
     })
     .catch(err => {
