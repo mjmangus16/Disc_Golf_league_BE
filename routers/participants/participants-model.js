b8ecee9a1e02a52be994e65f908cb3579b16557a
Michael Mangus <mjmangus16@yahoo.com>
2019-11-30 20:50:55 -0500

    added update and delete to participants model

--- routers/participants/participants-model.js
+++ routers/participants/participants-model.js
@@ -3,7 +3,8 @@ const db = require("../../data/dbConfig");
 module.exports = {
   getParticipants,
   getParticipantsByLeagueAndMember,
-  getParticipantsByRoundId
+  getParticipantsByRoundId,
+  deleteParticipant
 };
 
 function getParticipants() {
@@ -27,5 +28,26 @@ async function getParticipantsByRoundId(round_id) {
     "=",
     "participants.member_id"
   );
-  return participants.filter(p => p.round_id == round_id);
+  return participants
+    .filter(p => p.round_id == round_id)
+    .map(p => {
+      return {
+        f_name: p.f_name,
+        l_name: p.l_name,
+        score: p.score,
+        participant_id: p.participant_id
+      };
+    });
+}
+
+function updateParticipant(participant_id, changes) {
+  db("participants")
+    .where({ participant_id })
+    .update(changes, "*");
+}
+
+function deleteParticipant(participant_id) {
+  db("participants")
+    .where("participant_id", participant_id)
+    .del();
 }
