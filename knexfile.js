eb89a171ba669008653c4a66d15567a76e4e546e
Michael Mangus <mjmangus16@yahoo.com>
2019-12-19 11:33:06 -0500

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -37,18 +37,17 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
+    client: "pg",
+    connection: process.env.DATABASE_URL || {
+      database: "DB",
+      user: "user",
       password: "password"
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: "knex_migrations"
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
