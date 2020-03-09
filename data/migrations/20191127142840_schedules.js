bf079bb64e8522efd51e4aa8aceb37a97c43f37a
Michael Mangus <mjmangus16@yahoo.com>
2020-03-08 21:18:51 -0400

    changed the schedule migration

--- data/migrations/20191127142840_schedules.js
+++ data/migrations/20191127142840_schedules.js
@@ -2,11 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("schedules", schedules => {
     schedules.increments("schedule_id").unique();
     schedules.string("date").notNullable();
-    schedules.string("all");
-    schedules.string("rec");
-    schedules.string("int");
-    schedules.string("adv");
-    schedules.string("open");
+    schedules.string("info");
     schedules.integer("league_id").notNullable();
   });
 };
--- data/seeds/06_schedules.js
+++ data/seeds/06_schedules.js
@@ -8,91 +8,55 @@ exports.seed = function(knex) {
         {
           schedule_id: 1,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 2,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 3,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 4,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 5,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 6,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 7,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 8,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         },
         {
           schedule_id: 9,
           date: "20191125",
-          all: "",
-          rec: "",
-          int: "Beaver Island State Park",
-          adv: "Joseph Davis",
-          open: "",
+          info: "",
           league_id: 1
         }
       ]);
