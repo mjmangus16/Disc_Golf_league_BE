744bd0abe601d983a166bd0d8c9d6d2938366552
Michael Mangus <mjmangus16@yahoo.com>
2020-03-13 21:37:30 -0400

    changed description in standings_format

--- data/seeds/07_standings_format.js
+++ data/seeds/07_standings_format.js
@@ -8,12 +8,8 @@ exports.seed = function(knex) {
         {
           standings_format_id: 1,
           name: "points_A",
-          description: "This is the description for points_A"
-        },
-        {
-          standings_format_id: 2,
-          name: "points_B",
-          description: "This is the description for points_B"
+          description:
+            "This format awards points according to how many players participate in a round. Each player is awarded 1 point times how many positions from last they are. If there are 20 players. 1st place gets 20 points, 2nd is 19 .. 10th 10 .. and last place 1. Handles ties and doubles. Use team names for members if your tracking team doubles. If you want to track individual players doubles points, each member the score of the team. We will eventually add in the ability to drop worst rounds."
         }
       ]);
     });
